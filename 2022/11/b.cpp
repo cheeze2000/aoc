64c1f6d3568bad9555a15294c3317dce9748e7e5
cheeze2000 <me@cheeze2000.net>
2022-12-16 19:17:20 +0700

    refactor(2022): prefer no separate header

--- 2022/11/b.cpp
+++ 2022/11/b.cpp
@@ -1,4 +1,39 @@
-#include "../../io.hpp"
+#include <bits/stdc++.h>
+using namespace std;
+
+using ll = long long;
+
+int read_num() {
+	string xs;
+	char c;
+
+	while (cin.get(c)) {
+		if (isdigit(c)) {
+			xs.push_back(c);
+		} else {
+			if (xs.empty()) continue;
+			else return stoi(xs);
+		}
+	}
+}
+
+vector<int> read_nums() {
+	string xs, ys;
+	vector<int> zs;
+
+	getline(cin, xs);
+	xs.push_back(' ');
+	for (char x : xs) {
+		if (isdigit(x)) {
+			ys.push_back(x);
+		} else if (!ys.empty()) {
+			zs.push_back(stoi(ys));
+			ys.clear();
+		}
+	}
+
+	return zs;
+}
 
 struct M {
 	vector<ll> xs;
