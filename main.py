e26ef7f46640b1a677b7c4ac6c08093e56694f5c
cheeze2000 <me@cheeze2000.net>
2025-12-05 19:02:20 +0800

    time

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import importlib
 import sys
+import time
 
 argv = sys.argv
 year = argv[1]
@@ -9,6 +10,10 @@ input_type = argv[3] if len(argv) > 3 else "x"
 
 module = importlib.import_module(f"{year}.{day}.{day}{part}")
 input = open(f"{year}/{day}/{day}{input_type}.txt").read().strip()
+
+start = time.perf_counter_ns()
 answer = module.solve(input)
+end = time.perf_counter_ns()
 
 print(answer)
+print(f"⏲ {(end - start) / 1_000_000} ms")
