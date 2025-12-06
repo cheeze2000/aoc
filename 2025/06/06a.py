15596355a23dcaa2536e0a6d0884c77edc0e3fcb
cheeze2000 <me@cheeze2000.net>
2025-12-07 01:49:18 +0800

    2025 06

--- 2025/06/06a.py
+++ 2025/06/06a.py
@@ -1,2 +1,17 @@
+import math
+
 def solve(input):
-	pass
+	lines = input.splitlines()
+	nums = [line.split() for line in lines]
+	operators = nums.pop()
+
+	ans = 0
+
+	for i in range(len(operators)):
+		column = [int(row[i]) for row in nums]
+		if operators[i] == "+":
+			ans += sum(column)
+		else:
+			ans += math.prod(column)
+
+	return ans
--- 2025/06/06b.py
+++ 2025/06/06b.py
@@ -1,2 +1,34 @@
+import math
+
 def solve(input):
-	pass
+	lines = input.splitlines()
+	operators = lines.pop()
+	nums = lines
+
+	separator_indices = [i - 1 for i, op in enumerate(operators) if op != " " and i > 0]
+
+	for i in range(len(nums)):
+		row = list(nums[i])
+		for sep in separator_indices:
+			row[sep] = "|"
+
+		nums[i] = "".join(row).split("|")
+
+	ans = 0
+
+	operators = operators.split()
+	for i in range(len(operators)):
+		column = [row[i] for row in nums]
+		n = len(column[0])
+
+		transposed = []
+		for j in range(n - 1, -1, -1):
+			num = [entry[j] for entry in column if entry[j] != " "]
+			transposed.append(int("".join(num)))
+
+		if operators[i] == "+":
+			ans += sum(transposed)
+		else:
+			ans += math.prod(transposed)
+
+	return ans
