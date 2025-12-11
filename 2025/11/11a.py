521315b5183bfc77773f41d0c4cfcc5a655fd983
cheeze2000 <me@cheeze2000.net>
2025-12-11 21:20:49 +0800

    2025 11

--- 2025/11/11a.py
+++ 2025/11/11a.py
@@ -1,2 +1,24 @@
+from functools import cache
+
 def solve(input):
-	pass
+	lines = input.splitlines()
+
+	graph = {}
+
+	for line in lines:
+		[a, b] = line.split(": ")
+		graph[a] = b.split(" ")
+
+	@cache
+	def dfs(node):
+		if node == "out":
+			return 1
+
+		paths = 0
+
+		for neighbour in graph[node]:
+			paths += dfs(neighbour)
+
+		return paths
+
+	return dfs("you")
--- 2025/11/11b.py
+++ 2025/11/11b.py
@@ -1,2 +1,30 @@
+from functools import cache
+
 def solve(input):
-	pass
+	lines = input.splitlines()
+
+	graph = {}
+
+	for line in lines:
+		[a, b] = line.split(": ")
+		graph[a] = b.split(" ")
+
+	def count_paths(start, end):
+		@cache
+		def dfs(node):
+			if node == end:
+				return 1
+
+			paths = 0
+
+			for neighbour in graph.get(node, []):
+				paths += dfs(neighbour)
+
+			return paths
+
+		return dfs(start)
+
+	fft_dac = count_paths("svr", "fft") * count_paths("fft", "dac") * count_paths("dac", "out")
+	dac_fft = count_paths("svr", "dac") * count_paths("dac", "fft") * count_paths("fft", "out")
+
+	return fft_dac + dac_fft
